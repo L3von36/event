121cddd8834e85b8b6f12237674de5a3f28b8714
L3von36 <novelwolde36@gmail.com>
2026-01-30 11:25:46 +0300

    social connections

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -51,6 +51,9 @@ const Footer = () => {
               <a href="https://wa.me/251915843131" target="_blank" rel="noopener noreferrer" className="w-10 h-10 bg-gray-800 rounded-full flex items-center justify-center hover:bg-[#25D366] transition-colors group">
                 <Phone className="w-5 h-5 text-gray-400 group-hover:text-white" />
               </a>
+              <a href="https://www.facebook.com/share/1ArXYyhefP/?mibextid=wwXIfr" target="_blank" rel="noopener noreferrer" className="w-10 h-10 bg-gray-800 rounded-full flex items-center justify-center hover:bg-[#1877F2] transition-colors group">
+                <Facebook className="w-5 h-5 text-gray-400 group-hover:text-white" />
+              </a>
               <a href="#" className="w-10 h-10 bg-gray-800 rounded-full flex items-center justify-center hover:bg-primary-600 transition-colors group">
                 <Instagram className="w-5 h-5 text-gray-400 group-hover:text-white" />
               </a>
